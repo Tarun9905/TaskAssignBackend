cd7100daa72f52ededd900c1ad4a4635660cb6c6
Tarun9905 <ruykjr@gmail.com>
2025-05-06 19:00:46 +0530

    commit

--- TaskAssignProject/TaskAssignProject/settings.py
+++ TaskAssignProject/TaskAssignProject/settings.py
@@ -186,7 +186,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
 
 # Update ALLOWED_HOSTS to dynamically take values from the environment
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost 127.0.0.1').split()
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(" ")
 
 # Application definition
 
